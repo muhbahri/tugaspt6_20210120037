b867f5ba4e2f484d6bc07d6b11d8950f8bfa04fe
Muhamad Bahri <muhamadbahri0408@gmail.com>
2023-11-17 08:11:06 +0700

    add db user_login.php

---
+++ user_login.php
@@ -0,0 +1,32 @@
+<?php 
+  header("Access-Control-Allow-Origin: *");
+  header("Access-Control-Allow-Headers: *");
+  $con=mysqli_connect('localhost','root','','db_sample');
+
+$json =file_get_contents('php://input');
+ //Decoding the received JSON and store into $obj variable.
+ $obj = json_decode($json,true);
+ 
+  if(isset($obj["username"]) && isset($obj["password"])){
+    $uname =mysqli_real_escape_string($con,$obj['username']);
+    $pwd =mysqli_real_escape_string($con,$obj['password']);
+    //Declare array variable
+    $result=[];
+    //Select Query
+    $sql="SELECT * FROM users WHERE uname='{$uname}' and upass='{$pwd}'";
+    $res=$con->query($sql);
+    if($res->num_rows>0){
+      $row=$res->fetch_assoc();   
+      $result['loginStatus']=true;
+      $result['message']="Login Successfully";
+      $result["userInfo"]=$row;
+      }else{
+      $result['loginStatus']=false;
+      $result['message']="Invalid Login Details";
+      }
+ // Converting the array into JSON format.
+  $json_data=json_encode($result);
+ // Echo the $json.
+  echo $json_data;
+  }
+ ?>
